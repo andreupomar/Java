0770ffb0b73074769e3efc8fa0997c63de928c5c
Andreu Pomar <apomar@cifpfbmoll.eu>
2019-12-18 17:50:03 +0100

    Primer commit

---
+++ Javaej1/src/ej1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		System.out.println ("Introduzca cinco números, separados por intro:");
+		Scanner entrada = new Scanner (System.in);
+		int num1 = entrada.nextInt();
+		int num2 = entrada.nextInt();
+		int num3 = entrada.nextInt();
+		int num4 = entrada.nextInt();
+		int num5 = entrada.nextInt();
+		
+		System.out.println ("Los números introducidos son: "+num1+", "+num2+", "+num3+", "+num4+" y "+num5);
+		
+	}
+
+}
