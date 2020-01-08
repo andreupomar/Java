f0d2dd2bf80a67cda5859980937ab605fc721cf8
Andreu Pomar <apomar@cifpfbmoll.eu>
2020-01-08 18:22:52 +0100

    .

--- Javaej1/src/ej5.java
+++ Javaej1/src/ej5.java
@@ -7,23 +7,33 @@ public class ej5 {
 		
 		int[] tab1 = new int [10];
 		int[] tab2 = new int [10];
+		int[] tab3 = new int [20];
 		
 		int i = 0;
 		
 		for (i = 0; i < 10; i++) {
-			System.out.println ("Introduzca diez n�meros para el array 1, separados por intro:");
+			System.out.println ("Introduzca diez números para el array 1, separados por intro:");
 			Scanner entrada = new Scanner (System.in);
 			tab1[i] = entrada.nextInt();
 		}
 		
 		for (i = 0; i < 10; i++) {
-			System.out.println ("Introduzca diez n�meros para el array 2, separados por intro:");
+			System.out.println ("Introduzca diez números para el array 2, separados por intro:");
 			Scanner entrada = new Scanner (System.in);
 			tab2[i] = entrada.nextInt();
 		}
 		
-		for (i = 0; i < 10; i++) {
-			System.out.println (tab1[i]+" "+tab2[i]);
+		for (i = 0; i < 20; i++) { // Ni puta idea de por qué i/2, que a veces da decimales me funciona, pero aquí estamos ¯\_(ツ)_/¯
+			if (i%2==0) {
+				tab3[i] = tab1[i/2];
+			}
+			else {
+				tab3[i] = tab2[i/2];
+			}
+		}
+		
+		for (i = 0; i < 20; i++) {
+			System.out.println(tab3[i]);
 		}
 		
 		
