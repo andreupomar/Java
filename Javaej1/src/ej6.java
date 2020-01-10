f7237792e75f86a92f93f80b0c40e9062c1570c3
Andreu Pomar <apomar@cifpfbmoll.eu>
2020-01-10 16:58:59 +0100

    añadido ejercicio 6

--- Javaej1/src/ej6.java
+++ Javaej1/src/ej6.java
@@ -2,8 +2,37 @@
 public class ej6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String[] ser1 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+		String[] ser2 = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+		String[] resultado = new String[24];
+		
+		int i = 0;
+		int contador1 = 0;
+		int contador2 = 0;
+		boolean turno = false;
+		int metronomo = 0;
+		
+		for (i = 0; i < 24; i++) {
+			if (turno == false) {
+				resultado[i] = ser1[contador1];
+				contador1 ++;
+				metronomo ++;
+				}
+			
+			else{
+				resultado[i] = ser2[contador2];
+				contador2 ++;
+				metronomo ++;	
+			}
+			
+			if (metronomo == 3) {
+				turno = !turno;
+				metronomo = 0;
+			}
+			
+			System.out.println(resultado[i]);
 	}
 
+}
 }
